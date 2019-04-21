6f0c47955e92d2de6cf3faf71aba2fb6518579b5
Ilian Ranguelov <me@radarlog.net>
2019-04-21 17:57:05 +0200

    test PersistenceRepository against MySQL

--- tests/Infrastructure/MySql/PersistenceRepositoryTest.php
+++ tests/Infrastructure/MySql/PersistenceRepositoryTest.php
@@ -5,6 +5,7 @@ namespace Radarlog\S3Uploader\Tests\Infrastructure\MySql;
 
 use Radarlog\S3Uploader\Domain\Image;
 use Radarlog\S3Uploader\Domain\Repository;
+use Radarlog\S3Uploader\Infrastructure\MySql\Connection;
 use Radarlog\S3Uploader\Tests\FunctionalTestCase;
 
 class PersistenceRepositoryTest extends FunctionalTestCase
@@ -12,11 +13,25 @@ class PersistenceRepositoryTest extends FunctionalTestCase
     /** @var Repository */
     private $repository;
 
+    /** @var Connection */
+    private $connection;
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->repository = self::$container->get('test.repository');
+        $this->repository = self::$container->get('test.repository.mysql');
+
+        $this->connection = self::$container->get(Connection::class);
+
+        $this->connection->beginTransaction();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->connection->rollBack();
+
+        parent::tearDown();
     }
 
     public function testAddNew(): void
