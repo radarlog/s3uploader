abf333004fc72716e3ccff2c832c230632557d7a
Ilian Ranguelov <me@radarlog.net>
2020-08-19 00:52:09 +0200

    debug CI

--- tests/Frontend/ts/doop.test.ts
+++ tests/Frontend/ts/doop.test.ts
@@ -2,22 +2,16 @@ import $ from 'jquery';
 import fetch from 'node-fetch';
 
 describe('Doop Test Suite', () => {
-    const url = 'http://' + process.env.TRAVIS_APP_HOST;
+    const url = 'http://0.0.0.0';
 
     async function loadHtml() {
         const response = await fetch(url);
-
-        console.log(url);
 
         return response.text();
     }
 
     test('Upload file input has been rendered', async () => {
-        const html = await loadHtml()
-
-        console.log(html);
-
-        document.documentElement.innerHTML = html;
+        document.documentElement.innerHTML = await loadHtml();
 
         expect($('#upload_image').hasClass('custom-file-input')).toBeTruthy();
     });
